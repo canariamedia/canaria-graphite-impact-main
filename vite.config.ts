447c0b32f628cea2e29efa7b6cefc9cc23ed5acd
canariamedia <canaria.media0@gmail.com>
2025-12-02 11:02:11 -0300

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  // MUY IMPORTANTE → ESTA URL DEBE COINCIDIR CON LA DE GITHUB PAGES
   base: "/canaria-graphite-impact-main/",
   build: {
     outDir: "dist",
