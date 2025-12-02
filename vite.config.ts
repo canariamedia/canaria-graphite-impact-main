e455a740fa2d709fa3ccccd0f6e2cf3e8b54c3bb
canariamedia <canaria.media0@gmail.com>
2025-12-02 10:43:09 -0300

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-// 👇 AGREGAR ESTA LÍNEA
-import { fileURLToPath, URL } from "node:url";
-
 export default defineConfig({
   plugins: [react()],
-  // 👇 AGREGAR ESTE BLOQUE
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "/web/",
+  base: "/canaria-graphite-impact-main/",
   build: {
     outDir: "dist",
   },
